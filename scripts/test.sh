19d13dad4c2aef3351357d2f42bf009f12dd8ff5
Giedrius <giedrius.medzevicius@gmail.com>
2018-03-13 15:58:52 +0200

    tmp: add more debug

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,9 +9,13 @@ fi
 RESTSERVER_PID=$!
 echo $RESTSERVER_PID
 curl http://localhost:8888/endpoints
+ps aux | grep rest
+netstat -nlp | grep rest
 
 cd tests-rest && npm install && npm test
 TEST_STATUS=$?
+ps aux | grep rest
+netstat -nlp | grep rest
 
 kill -2 $RESTSERVER_PID
 sleep 1
