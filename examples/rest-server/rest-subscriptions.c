9b371ed26991daa45d60168874f5f006a90d1ccb
Tomas Markevičius <tomas.m@8devices.com>
2018-03-16 13:33:11 +0200

    Add observation removal implementation with DELETE requests

--- examples/rest-server/rest-subscriptions.c
+++ examples/rest-server/rest-subscriptions.c
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 #include <string.h>
 
 #include "restserver.h"
@@ -54,6 +53,14 @@ static void rest_observe_cb(uint16_t clientID, lwm2m_uri_t *uriP, int count, lwm
             data, dataLength);
 
     rest_notify_async_response(ctx->rest, response);
+}
+
+static void rest_unobserve_cb(uint16_t clientID, lwm2m_uri_t *uriP, int count, lwm2m_media_type_t format,
+                              uint8_t *data, int dataLength, void *context)
+{
+    rest_observe_context_t *ctx = (rest_observe_context_t *)context;
+
+    fprintf(stdout, "[UNOBSERVE-RESPONSE] id=%s data=%p\n", ctx->response->id, data);
 }
 
 int rest_subscriptions_put_cb(const ulfius_req_t *req, ulfius_resp_t *resp, void *context)
@@ -180,5 +187,106 @@ exit:
     }
 
     return err;
+}
+
+int rest_subscriptions_delete_cb(const ulfius_req_t *req, ulfius_resp_t *resp, void *context)
+{
+    rest_context_t *rest = (rest_context_t *)context;
+    const char *name;
+    lwm2m_client_t *client;
+    char path[100];
+    size_t len;
+    lwm2m_uri_t uri;
+    json_t *jresponse;
+    lwm2m_observation_t *targetP;
+    rest_observe_context_t *observe_context = NULL;
+    int res;
+
+    /*
+     * IMPORTANT!!! Error handling is split into two parts:
+     * First, validate client request and, in case of an error, fail fast and
+     * return any related 4xx code.
+     * Second, once the request is validated, start allocating neccessary
+     * resources and, in case of an error, jump (goto) to cleanup section at
+     * the end of the function.
+     */
+
+    /* Find requested client */
+    name = u_map_get(req->map_url, "name");
+    client = rest_endpoints_find_client(rest->lwm2m->clientList, name);
+    if (client == NULL)
+    {
+        ulfius_set_empty_body_response(resp, 404);
+        return U_CALLBACK_CONTINUE;
+    }
+
+    /* Reconstruct and validate client path */
+    len = snprintf(path, sizeof(path), "/subscriptions/%s/", name);
+
+    if (req->http_url == NULL || strlen(req->http_url) >= sizeof(path) || len >= sizeof(path))
+    {
+        fprintf(stderr, "%s(): invalid http request (%s)!\n", __func__, req->http_url);
+        return U_CALLBACK_ERROR;
+    }
+
+    // this is probaly redundant if there's only one matching ulfius filter
+    if (strncmp(path, req->http_url, len) != 0)
+    {
+        ulfius_set_empty_body_response(resp, 404);
+        return U_CALLBACK_CONTINUE;
+    }
+
+    /* Extract and convert resource path */
+    strcpy(path, &req->http_url[len-1]);
+
+    if (lwm2m_stringToUri(path, strlen(path), &uri) == 0)
+    {
+        ulfius_set_empty_body_response(resp, 404);
+        return U_CALLBACK_CONTINUE;
+    }
+
+    /*
+     * IMPORTANT! This is where server-error section starts and any error must
+     * go through the cleanup section. See comment above.
+     */
+    const int err = U_CALLBACK_ERROR;
+
+    // Search existing registrations to confirm existing observation
+    for (targetP = client->observationList; targetP != NULL; targetP = targetP->next)
+    {
+        if (targetP->uri.objectId == uri.objectId
+         && targetP->uri.flag == uri.flag
+         && targetP->uri.instanceId == uri.instanceId
+         && targetP->uri.resourceId == uri.resourceId)
+        {
+            observe_context = targetP->userData;
+            break;
+        }
+    }
+
+    if (observe_context == NULL)
+    {
+        ulfius_set_empty_body_response(resp, 404);
+        return U_CALLBACK_CONTINUE;
+    }
+
+    res = lwm2m_observe_cancel(
+            rest->lwm2m, client->internalID, &uri,
+            rest_unobserve_cb, observe_context
+    );
+
+    if (res != 0)
+    {
+        return err;
+    }
+
+    rest_list_remove(rest->observeList, observe_context->response);
+
+    jresponse = json_object();
+    json_object_set_new(jresponse, "async-response-id", json_string(observe_context->response->id));
+    ulfius_set_json_body_response(resp, 202, jresponse);
+    json_decref(jresponse);
+
+    return U_CALLBACK_CONTINUE;
 }
 
