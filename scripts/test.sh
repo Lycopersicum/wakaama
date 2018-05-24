fb2a4db4983732362be9f24525f45b1f636ecf7b
Tomas Markevičius <tomas.m@8devices.com>
2018-05-24 11:45:02 +0300

    Add HTTPS connection tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,13 +5,23 @@ then
     exit 1
 fi
 
+openssl genrsa -out private.key 2048
+openssl req -days 365 -out certificate.pem -new -x509 -key private.key -subj '/CN=localhost'
+
+openssl genrsa -out other_private.key 2048
+openssl req -days 365 -out other_certificate.pem -new -x509 -key other_private.key -subj '/CN=localhost'
+
 ./build/restserver > /dev/null &
 RESTSERVER_PID=$!
+
+./build/restserver -c ./tests-rest/secure.cfg > /dev/null &
+SECURE_RESTSERVER_PID=$!
 
 cd tests-rest && npm install && npm test
 TEST_STATUS=$?
 
 kill -2 $RESTSERVER_PID
+kill -2 $SECURE_RESTSERVER_PID
 sleep 1
 
 exit $TEST_STATUS
