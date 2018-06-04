5d70a65d5d015f91359e70465c4c63805e787f3f
Tomas Markevičius <tomas.m@8devices.com>
2018-06-04 14:20:14 +0300

    Change .gcda files location, merge both coverage results

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,10 +11,10 @@ openssl req -days 365 -out certificate.pem -new -x509 -key private.key -subj '/C
 openssl genrsa -out other_private.key 2048
 openssl req -days 365 -out other_certificate.pem -new -x509 -key other_private.key -subj '/CN=localhost'
 
-GCOV_PREFIX=regular ./build/restserver > /dev/null &
+GCOV_PREFIX_STRIP=1 GCOV_PREFIX=regular ./build/restserver > /dev/null &
 RESTSERVER_PID=$!
 
-GCOV_PREFIX=secure ./build/restserver -c ./tests-rest/secure.cfg > /dev/null &
+GCOV_PREFIX_STRIP=1 GCOV_PREFIX=secure ./build/restserver -c ./tests-rest/secure.cfg > /dev/null &
 SECURE_RESTSERVER_PID=$!
 
 cd tests-rest && npm install && npm test
@@ -23,5 +23,7 @@ TEST_STATUS=$?
 kill -2 $RESTSERVER_PID
 kill -2 $SECURE_RESTSERVER_PID
 sleep 1
+
+gcov-tool merge -o ./ regular secure
 
 exit $TEST_STATUS
